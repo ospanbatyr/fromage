adapt repo for mistral, fix low-precision training

fromage/experiment.py
@@ -29,21 +29,27 @@ class Experiment(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         pixels, text = batch
         opt = self.optimizers()
+        opt.zero_grad()
+
         opt_config = self.config['optimizer']
+        grad_acc_step = opt_config['grad_acc_step']
 
         losses = {f"{mode}_loss/train":0 for mode in self.modes}
         for mode in self.modes:
+            opt.zero_grad()
             output, t2i_embs, i2t_embs, full_labels, last_logits = self.forward(pixels, text, mode=mode)
-            loss = output.loss
+            loss = output.loss / grad_acc_step
 
             if mode == "retrieval":
                 logits_per_image, logits_per_text = get_logits(t2i_embs, i2t_embs)
-                loss += retrieval_loss(logits_per_image, logits_per_text)
+                loss += retrieval_loss(logits_per_image, logits_per_text) / grad_acc_step
 
-            opt.zero_grad()
             self.manual_backward(loss)
-            self.clip_gradients(opt, gradient_clip_val=opt_config['gradient_clip_val'], gradient_clip_algorithm="norm")
-            opt.step()
+
+            if (batch_idx + 1) % grad_acc_step == 0:
+                self.clip_gradients(opt, gradient_clip_val=opt_config['gradient_clip_val'], gradient_clip_algorithm="norm")
+                opt.step()
+                opt.zero_grad()
 
             if mode == "caption":
                 log_acc_dict = mode_accuracy(mode=mode, output=output.logits, full_labels=full_labels)
@@ -117,7 +123,16 @@ class Experiment(pl.LightningModule):
         opt_config = self.config['optimizer']
         opt_name = opt_config['algorithm']
         opt_params = opt_config['params']
+        
+        print("Total params:", sum(p.numel() for p in self.model.parameters()))
+
+        # Model is loaded in fp16, all trainable params should be fp16
+        self.model.model.lm.lm_head.weight.data = self.model.model.lm.lm_head.weight.data.to(dtype=torch.float32)
+        self.model.model.lm.model.embed_tokens.weight.data = self.model.model.lm.model.embed_tokens.weight.data.to(dtype=torch.float32)
+
         parameters = filter(lambda p: p.requires_grad, self.model.parameters())
+
+        print("Trainable params:", sum(p.numel() for p in self.model.parameters() if p.requires_grad))
 
         if opt_name == "AdamW":
             optimizer = optim.AdamW(parameters, **opt_params)

fromage/model.py
@@ -87,20 +87,25 @@ class FromageModel(nn.Module):
     def _init_language_model(self) -> None:
         # create language model
         model_checkpoint = self.config['language_model']
-        self.lm = AutoModelForCausalLM.from_pretrained(model_checkpoint)
+
+        if "7B" in model_checkpoint:
+            self.lm = AutoModelForCausalLM.from_pretrained(model_checkpoint, torch_dtype=torch.float16)
+        else:
+            self.lm = AutoModelForCausalLM.from_pretrained(model_checkpoint)
+
 
         # freeze the language model
-        for param in self.lm.parameters():
+        for name, param in self.lm.named_parameters():
             param.requires_grad = False
-
-        self.lm.eval()
-
+            
         # resize token embeddings (as we added [RET] token) and 
         # get input embeddings as we will process information on embedding level, not index level 
         self.lm.resize_token_embeddings(len(self.tokenizer))
         self.input_embeddings = self.lm.get_input_embeddings()
 
         self.lm_embed_dim = self.input_embeddings.embedding_dim
+
+        self.lm.eval()
 
 
     def _init_image_encoder(self) -> None:
@@ -328,6 +333,9 @@ class Fromage(nn.Module):
         tokenizer.add_tokens("[RET]")
         ret_id = tokenizer('[RET]', add_special_tokens=False).input_ids
         assert len(ret_id) == 1, "Failed to add [RET] token to tokenizer"
+
+        if tokenizer.pad_token is None:
+            tokenizer.pad_token = tokenizer.eos_token
 
         ret_token_idx = ret_id[0]
         self.tokenizer = tokenizer