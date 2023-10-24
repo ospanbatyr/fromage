e6ed85c301d65eb6334cd07fc50962011e25b45c
Osman Batur İnce <osmanbaturince@outlook.com>
2023-10-24 15:42:13 +0300

    fixed bus in VQAEval, better eval

--- eval/VQAEval.py
+++ eval/VQAEval.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Tutorial & Evaluation
-
-# In[1]:
-
-
 import sys
 import torch
 import yaml
@@ -17,124 +9,55 @@ from fromage.experiment import Experiment
 from fromage.data import MIMICDataset, cxr_image_transform
 from fromage.utils import preprocess_report
 from evaluate import load # if throws error, please run the following command "pip instal evaluate"
+import string 
 
 
-# In[2]:
-
-
-## config
+# VARIABLES
 ckpt_path = "../logs/checkpoints/lm_gen_vis_med_mistral_rerun2/last.ckpt"
 config_path = "../config/train-untied_lm_gen_vis_med.yaml"
 dataset_path = "../data/datasets/VQA_RAD"
-MAX_LEN = int(sys.argv[1])
+MAX_LEN_VQA_CLOSED = int(sys.argv[1])
+MAX_LEN_VQA_OPEN = int(sys.argv[2])
+bleu_metric = load("bleu")
 
-
-# In[3]:
-
-
-transform = cxr_image_transform(resize=512, center_crop_size=480, train=False) 
-dataset = VQA_RADDataset(dataset_path, transform)
-
-
-# In[4]:
-
-
-dataset[0] # returns image, question, answer
-
-
-# In[ ]:
-
-
+# LOAD MODEL
 with open(config_path) as file:
     config = yaml.safe_load(file)
 
 device = "cuda"
-
 model = Experiment(config)
 model = model.load_from_checkpoint(ckpt_path)
 model = model.model.to(device)
 model.device = device
 
-# In[ ]:
-
-
-
-
-img, question, answer = dataset[0]
-prompt = str("Question: " + question + " Answer: ")
-print("Prompt: ", prompt)
-
 model.eval()
 
-print("Total params:", sum(p.numel() for p in model.parameters()))
-
-# for name, p in model.named_parameters():
-#     print(name, p.requires_grad, p.data.dtype)
-
-parameters = filter(lambda p: p.requires_grad, model.parameters())
-
-print("Trainable params:", sum(p.numel() for p in model.parameters() if p.requires_grad))
-
-with torch.inference_mode() as inf_mode, torch.autocast(device_type="cuda") as cast:
-    prompts = [img, prompt] 
-    print("Model Answer: ", model.generate_for_images_and_texts(prompts, max_len=MAX_LEN)) # top_p=0.9, temperature=0.5
-    
-print("Correct Answer: ", answer)
-
-
-# # Evaluation
-
-# In[ ]:
-
-
-transform = cxr_image_transform(resize=512, center_crop_size=480, train=False) 
 dataset_closed = VQA_RADDataset(dataset_path, transform, 'closed')
 dataset_open = VQA_RADDataset(dataset_path, transform, 'open')
 
-print(dataset_closed.get_len())
-print(dataset_open.get_len())
-
-
-# ## Closed dataset: accuracy
-
-# In[ ]:
-
-
-import string 
+print("VQA-RAD Closed Length: ", dataset_closed.get_len())
+print("VQA-RAD Open Length: ", dataset_open.get_len())
 
 right_answers = 0
 total_answers = 0
-
-def get_model_response(prompts, max_len):
-    model_ans_full = model.generate_for_images_and_texts(prompts, max_len=max_len) # top_p=0.9, temperature=0.5
-    model_ans = model_ans_full.translate(str.maketrans('', '', string.punctuation)) # remove punctuation
-    return model_ans
 
 for i, idx in enumerate(tqdm(dataset_closed)):
     img, q, ans = idx 
     with torch.inference_mode() as inf_mode, torch.autocast(device_type="cuda") as cast:
         model.eval()
         prompts = [idx[0], str("Question: " + idx[1] + "Answer (Yes or No): ")] 
-        model_ans = get_model_response(prompts, max_len=MAX_LEN)
+        model_ans = model.generate_for_images_and_texts(prompts, max_len=MAX_LEN_VQA_CLOSED) # top_p=0.9, temperature=0.5
+        model_ans = model_ans.translate(str.maketrans('', '', string.punctuation)) # remove punctuation
+        
         if i % 50 == 1:
             print(model_ans.lower(), ans.lower())
         if model_ans.lower() == ans.lower():
             right_answers += 1
         total_answers += 1        
 
-print(right_answers, '/', total_answers)
-print((right_answers/total_answers)*100, '% correct')
-
-# ## Open dataset: Bleu score
-
-# In[ ]:
-
-
-exact_match_metric = load("bleu")
-
-
-# In[ ]:
-
+print("VQA-RAD Evaluation")
+print("="*30)
+print(f"Closed (Yes/No) Question Accuracy: {right_answers} / {total_answers} = {(right_answers / total_answers)*100}")
 
 total_bleu_score = 0
 total = 0
@@ -144,18 +67,15 @@ for idx in tqdm(dataset_open):
     with torch.inference_mode() as inf_mode, torch.autocast(device_type="cuda") as cast:
         model.eval()
         prompts = [idx[0], str("Question: " + idx[1] + " Answer: ")] 
-        model_ans_full = model.generate_for_images_and_texts(prompts, max_len=MAX_LEN) # top_p=0.9, temperature=0.5    
-        current_bleu_scores = []
-        for _ in range(4): # try 5 times, get the best score of those 5 times
+        model_ans = model.generate_for_images_and_texts(prompts, max_len=MAX_LEN_VQA_OPEN) # top_p=0.9, temperature=0.5    
+        max_bleu_score = 0
+        for _ in range(5): # try 5 times, get the best score of those 5 times
             try:
-                bleu_score = exact_match_metric.compute(predictions=[model_ans_full], references=[ans]).get('bleu')
-                current_bleu_scores.append(bleu_score)
+                bleu_score = bleu_metric.compute(predictions=[model_ans], references=[ans]).get('bleu')
+                max_bleu_score = max(max_bleu_score, bleu_score)
             except:
                 pass
-        if len(current_bleu_scores) > 1:
-            total_bleu_score += max(current_bleu_scores) # you can also take the average
-        total += 1
-        
-print(total_bleu_score, '/', total)
-print("bleu score: ", total_bleu_score/total)
 
+    total += 1
+
+print(f"Open Question BLEU Score : {total_bleu_score} / {total} = {(total_bleu_score / total) * 100}")
